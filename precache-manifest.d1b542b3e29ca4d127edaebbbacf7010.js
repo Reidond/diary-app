3c69a0147a9995b6d60b160c8d5bcafd7896cba7
Reidond <reidond@gmail.com>
2019-11-07 21:25:38 +0200

    Updates

---
+++ precache-manifest.d1b542b3e29ca4d127edaebbbacf7010.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ced22b088f3dc916042ff8763b0b47b",
+    "url": "/diary-app/index.html"
+  },
+  {
+    "revision": "0994911a62a781940d70",
+    "url": "/diary-app/static/css/main.c5e4b970.chunk.css"
+  },
+  {
+    "revision": "800bc993614e9aa5d5b1",
+    "url": "/diary-app/static/js/2.2bdaf811.chunk.js"
+  },
+  {
+    "revision": "0994911a62a781940d70",
+    "url": "/diary-app/static/js/main.8582e9d5.chunk.js"
+  },
+  {
+    "revision": "d5ee3ad46311b955c88a",
+    "url": "/diary-app/static/js/runtime-main.0d69d9e7.js"
+  }
+]);
